c2450292fee367bb2001f2e99d66ae683293b057
JavierPeDe <javip107@gmail.com>
2021-10-29 18:57:32 +0200

    Appjs changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,63 @@
+import React, { useState } from 'react';
+import {
+  Icon,
+  NavbarContainer,
+  NavbarWrapper,
+  NavbarItems,
+  MenuItem,
+  MenuItemLink,
+  BurguerIcon,
+} from './menu.style';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { AboutMe } from './components/AboutMe';
+import { Home } from './components/Home';
+import { Projects } from './components/Projects';
+import { ContactMe } from './components/ContactMe';
 export const App = () => {
-  return <div className="App">Hello</div>;
+  const [showMenu, setShowMenu] = useState(false);
+  const handleOnClick = () => {
+    setShowMenu(!showMenu);
+  };
+  return (
+    <div className="App">
+      <NavbarContainer>
+        <NavbarWrapper>
+          <Icon>
+            <h1>{'<JPerez/>'} </h1>
+          </Icon>
+          <BurguerIcon onClick={() => handleOnClick()}>
+            {showMenu ? <FaTimes /> : <FaBars />}
+          </BurguerIcon>
+          <NavbarItems showMenu={showMenu}>
+            <div onClick={() => handleOnClick()}></div>
+            <MenuItem onClick={() => handleOnClick()}>
+              <MenuItemLink href="#home">HOME </MenuItemLink>
+            </MenuItem>
+            <MenuItem onClick={() => handleOnClick()}>
+              <MenuItemLink href="#aboutme">ABOUT ME</MenuItemLink>
+            </MenuItem>
+            <MenuItem onClick={() => handleOnClick()}>
+              <MenuItemLink href="#projects">PROJECTS </MenuItemLink>
+            </MenuItem>
+            <MenuItem onClick={() => handleOnClick()}>
+              <MenuItemLink href="#contactme">CONTACT ME </MenuItemLink>
+            </MenuItem>
+          </NavbarItems>
+        </NavbarWrapper>
+      </NavbarContainer>
+
+      <section id="home">
+        <Home />
+      </section>
+      <section id="aboutme">
+        <AboutMe />
+      </section>
+      <section id="projects">
+        <Projects />
+      </section>
+      <section id="contactme">
+        <ContactMe />
+      </section>
+    </div>
+  );
 };
